4a25996c8fb3781568dd46de56c24c5c1489a21a
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-12-25 03:25:31 +0200

    source files for a new project to practice creating registation and authentification

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { AuthModule } from './auth/auth.module';
+import { TransactionModule } from './transaction/transaction.module';
+
+@Module({
+  imports: [UserModule, CategoryModule, AuthModule, TransactionModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
